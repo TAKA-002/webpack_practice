e5b2b4328c15a767385aa513b5e20e1512aa5855
Takayuki Miura <6015abc@gmail.com>
2024-02-23 01:11:07 +0900

    ブラウザ検証ツールででていたReactのエラーを解消詳しくは以下。
    https://qiita.com/masaa0802/items/c56e1e255445b2333f84

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,13 +1,13 @@
-import ReactDom from "react-dom";
 import React from "react";
+import { createRoot } from "react-dom/client";
 
 const App = () => {
   return <div>Hello, React!!!</div>;
 };
 
-const root = document.getElementById("react-root");
-if (root) {
-  ReactDom.render(<App />, root);
+const container = document.getElementById("react-root");
+if (container) {
+  createRoot(container).render(<App />);
 } else {
   console.log("No react-root element found.");
 }
