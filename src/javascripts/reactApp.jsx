88fb128faedaa4ae43f3af85d92cd60b89e10736
Takayuki Miura <6015abc@gmail.com>
2024-02-23 19:31:05 +0900

    78ReactコンポーネントをTypescriptで書いてみる

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Alert from "./Alert.tsx";
 
 const App = () => {
-  return <div>Hello, React!!!</div>;
+  return (
+    <div>
+      Hello, React!!!
+      <Alert message="success!!" />
+    </div>
+  );
 };
 
 const container = document.getElementById("react-root");
